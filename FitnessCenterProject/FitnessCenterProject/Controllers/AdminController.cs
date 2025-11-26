b2d01cc40343d8c39688912c80e94dccb7b2e7d8
Petek İrem HIZLI <162015902+petekiremhizli@users.noreply.github.com>
2025-11-26 20:14:27 +0300

    Spor salonu web sitesi içintemel sayfalar genel oluşturuldu ve geliştirildi.

---
+++ FitnessCenterProject/FitnessCenterProject/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Antrenorler()
+        {
+            return View();
+        }
+        public IActionResult Hizmetler()
+        {
+            return View();
+        }
+        public IActionResult Salonlar()
+        {
+            return View();
+        }
+        public IActionResult Randevular()
+        {
+            return View();
+        }
+    }
+}
